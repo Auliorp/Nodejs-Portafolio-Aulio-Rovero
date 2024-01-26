da581bfb446664fd3b7877feeb7c38ee0f1f0cfa
Aulio Rovero <aulioroveroparedes@gmail.com>
2024-01-25 21:22:02 -0300

    se elimino metodo anterior con express y se remplazo por el de la documentacion de resend.

--- index.js
+++ index.js
@@ -1,41 +1,3 @@
-/* import express from 'express';
-import { Resend } from 'resend';
-import dotenv from 'dotenv';
-
-const app = express();
-dotenv.config();
-
-const apiKey = process.env.API_KEY;
-console.log(apiKey);
-const resend = new Resend(apiKey);
-
-// Ruta para enviar el correo electrónico
-app.get('/send-email', async (req, res) => {
-  try {
-    const { data, error } = await resend.emails.send({
-      from: 'Ahora si <onboarding@resend.dev>',
-      to: ['aulioroveroparedes@gmail.com'],
-      subject: 'De nuevo probando',
-      html: '<strong>Vamos!</strong>',
-    });
-
-    if (error) {
-      return res.status(500).json({ error: 'Error al enviar el correo electrónico' });
-    }
-
-    return res.status(200).json({ data });
-  } catch (err) {
-    console.error(err);
-    return res.status(500).json({ error: 'Error interno del servidor' });
-  }
-});
-
-// Iniciar el servidor Express
-
-app.listen(3000, () => {
-  console.log(`Servidor escuchando en el puerto ${port}`);
-}); */
-
 import express from 'express';
 import { Resend } from 'resend';
 import dotenv from 'dotenv'
@@ -43,10 +5,17 @@ import dotenv from 'dotenv'
 dotenv.config();
 const app = express();
 const apiKey = process.env.API_KEY;
-console.log(apiKey);
+
+if (!apiKey) {
+  console.error('API_KEY no está configurada. Asegúrate de proporcionarla en el archivo .env.');
+  process.exit(1);
+}
+
 const resend = new Resend(apiKey);
 
-app.get('/', async (req, res) => {
+app.use(express.json());
+
+app.get('/Send-mail', async (req, res) => {
   try {
     const { data, error } = await resend.emails.send({
       from: 'Express Docu <onboarding@resend.dev>',
