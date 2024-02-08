06c5cbf11f700ec027d2bd67302f27221bfec950
Aulio Rovero <aulioroveroparedes@gmail.com>
2024-02-08 19:12:34 -0300

    Correcciones en endpoint y environment

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ import cors from 'cors';
 dotenv.config();
 const app = express();
 const apiKey = process.env.API_KEY;
-
+const endpointMailForm = process.env.ENDPOINT_MAIL_FORM;
+const endpointCors = process.env.ENDPOINT_CORS
+const endpointLocal = process.env.ENDPOINT_LOCAL
+const myEmail = process.env.My_EMAIL
 if (!apiKey) {
   console.error('API_KEY no está configurada. Asegúrate de proporcionarla en el archivo .env.');
   process.exit(1);
@@ -15,7 +18,7 @@ if (!apiKey) {
 const resend = new Resend(apiKey);
 
 const corsOptions = {
-  origin: ['https://porfolio-aulio-rovero.vercel.app', 'http://localhost:5173'],  // Reemplaza con el dominio de tu aplicación React
+  origin: [endpointCors, endpointLocal],
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
 };
@@ -26,13 +29,13 @@ app.use(cors(corsOptions));
 // Middleware para analizar el cuerpo de solicitudes con formato JSON
 app.use(express.json());
 
-app.post('/Send-mail', async (req, res) => {
+app.post(endpointMailForm, async (req, res) => {
 
   const { Name, mail, Description } = req.body;
   try {
     const { data, error } = await resend.emails.send({
       from: 'Desde mi Portafolio <onboarding@resend.dev>',
-      to: ['aulioroveroparedes@gmail.com'],
+      to: [myEmail],
       subject: 'Formulario Portafolio',
       html: `<strong>Name:</strong> ${Name}<br><strong>Email:</strong> ${mail}<br><strong>Description:</strong> ${Description}`,
     });
